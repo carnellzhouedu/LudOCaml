1f6c7a948a4aef9793da428cff26a2729832548d
Carnell Zhou <67295397+carnellzhouedu@users.noreply.github.com>
2022-02-01 19:22:10 -0500

    Delete src directory

--- src/authors.mli
+++
@@ -1,8 +0,0 @@
-(** CS 3110 Fall 2021 Final Project
-
-    @author Taha Jafry (taj43)
-    @author Carnell Zhou (cz375)
-    @author Zhen Luan (zl784) *)
-
-val hours_worked : int
-(** [hours_worked] is the number of hours you worked on this assignment. *)
